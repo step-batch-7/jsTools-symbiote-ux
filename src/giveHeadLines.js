166f11c01d25cf4bf36e658db727d070a8ea4b2b
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-23 12:18:51 +0530

    Updated giveHeadLines function
    if file contains less than ten lines it will give the whole lines
    otherwise by default it will give ten lines

--- src/giveHeadLines.js
+++ src/giveHeadLines.js
@@ -1,4 +1,5 @@
 const giveHeadLines = function(fileContent) {
+  if (fileContent["lines"].length <= 10) return fileContent["lines"].join("\n");
   return fileContent["lines"].slice(0, 10).join("\n");
 };
 
