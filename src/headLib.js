a5754addef998a2907c1d88fa39c5700d6a23c5e
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-24 18:20:12 +0530

    Removed coverage

--- src/headLib.js
+++ src/headLib.js
@@ -8,18 +8,18 @@ const parseUserOptions = function(usrArgs) {
   return { count: 10, filePath: usrArgs[2] };
 };
 
-const loadLines = function(read, isFileExists, userOptions, encoding) {
-  if (!isFileExists(userOptions.filePath)) {
-    return { error: `head : ${userOptions.filePath} : no such file or directory`, lines: "" };
+const loadLines = function(read, isFileExists, filePath, encoding) {
+  if (!isFileExists(filePath)) {
+    return { error: `head : ${filePath} : no such file or directory`, lines: "" };
   }
-  const fileContent = read(userOptions.filePath, encoding);
+  const fileContent = read(filePath, encoding);
   return { lines: fileContent.split("\n") };
 };
 
 const head = function(usrArgs, read, isFileExists) {
   const userOptions = parseUserOptions(usrArgs);
   if (userOptions.error) return userOptions;
-  const fileContent = loadLines(read, isFileExists, userOptions, "utf8");
+  const fileContent = loadLines(read, isFileExists, userOptions.filePath, "utf8");
   if (fileContent.error) return fileContent;
   return { lines: fileContent["lines"].slice(0, userOptions.count).join("\n"), error: "" };
 };
