1ad5fa095449dbbaa785388d60569004acbe60a2
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2019-12-27 12:07:39 +0530

    Removed parseUserArgs file

--- src/headLib.js
+++ src/headLib.js
@@ -1,4 +1,16 @@
-const { parseUserOptions } = require("./parseUserArgs");
+const parseUserOptions = function(usrArgs) {
+  const noOfLinesGiven = usrArgs[2].slice(0, 2) == "-n" || Number.isInteger(+usrArgs[2]);
+  const count = +usrArgs[2].slice(-1) || +usrArgs[3];
+  const countIsInteger = Number.isInteger(count);
+  const errorMsg = { error: `head : ${usrArgs[3]} illegal count`, lines: "" };
+  const defaultOptions = { count: 10, filePath: usrArgs[2] };
+  const userOptions = { count: count, filePath: usrArgs[usrArgs.length - 1] };
+  if (noOfLinesGiven) {
+    if (!(countIsInteger && count != 0)) return errorMsg;
+    return userOptions;
+  }
+  return defaultOptions;
+};
 
 const loadLines = function(read, isFileExists, filePath, encoding) {
   if (!isFileExists(filePath)) {
@@ -16,4 +28,4 @@ const head = function(usrArgs, read, isFileExists) {
   return { lines: fileContent["lines"].slice(0, userOptions.count).join("\n"), error: "" };
 };
 
-module.exports = { loadLines, head };
+module.exports = { loadLines, head, parseUserOptions };
